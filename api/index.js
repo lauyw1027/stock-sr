a9c9c2570315431a9b204eb0a830609af923fc17
admin <>
2026-07-06 16:28:27 +0800

    Refactor project structure for Vercel serverless deployment14

--- api/index.js
+++ api/index.js
@@ -1,2 +1,5 @@
-import app from "../dist/index.js";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const app = require("../dist/index.js");
+export default app;
 export default app;
